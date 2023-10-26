05c925543bcc07bf7bb270ca1d7ce5095ea70a68
Noaa Maman <noaamaman325158@gmail.com>
2023-10-26 05:23:21 +0300

    Extract some random number appearance inside some web page.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
-
+import time
+import re
 def get_driver():
+    """
+    Define all the options and the configuration
+     of working with scraping and automations with the browser
+    :return:
+    """
     #Set options to make browsing easier
     options = webdriver.ChromeOptions()
     options.add_argument("disable-infobars")
@@ -14,11 +20,23 @@ def get_driver():
     driver.get("http://automated.pythonanywhere.com")
     return driver
 
+def clean_text(text):
+    """
+        Extract only the temperature from the input text
+    :param text:
+    :return:
+    """
+    #The Strategy of using string's approach is less recommended
+    # output = text.split(": ")
+    # return output[1]
+    return re.findall(r'\d+', text)[0]
 
 def main():
+    #Loading page .....
     driver = get_driver()
-    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-    return element.text
+    time.sleep(2)
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    return clean_text(element.text)
 print(main())
 
 
